f9e635cdc813ef8991196d3ad18d7ae35728bf92
Anastasiya Zubkova <AVlZubkova@sberbank.ru>
2022-03-30 21:55:44 +0300

    bst kinda done....

--- bst.hpp
+++ bst.hpp
@@ -13,7 +13,7 @@ namespace ft {
 		bst_node	*right;
 		bst_node	*parent;
 
-		bst_node(T value, bst_node *lt, bst_node *rt, bst_node *pt)
+		bst_node(T value, bst_node *lt = NULL, bst_node *rt = NULL, bst_node *pt = NULL)
 		: value(value), left(lt), right(rt), parent(pt) {};
 	};
 
@@ -21,6 +21,7 @@ namespace ft {
 	class bst_iterator : public iterator_traits<T*> {
 		private:
 		friend class bs_tree<T>;
+		friend bool operator==(const bst_iterator<T>& lhs, const bst_iterator<T>& rhs);
 
 		bst_node<T>	*position;
 		bs_tree<T>	*tree;
@@ -32,8 +33,6 @@ namespace ft {
 		bst_iterator() : position(NULL), tree(NULL) {};
 		bst_iterator(const bst_iterator& other) : position(other.position), tree(other.tree) {};
 		~bst_iterator() {};
-
-		const bst_node<T>		*getPosition() const { return this->position; }
 
 		bst_iterator	&operator=(const bst_iterator& other) {
 			this->position = other.position;
@@ -53,24 +52,57 @@ namespace ft {
 				while (this->position->left ! = NULL)
 					this->position = this->position->left;
 			} else {
-				bst_node<T>	*tmp = this->position;
-				while (this->position->parent != NULL) {
-					this->position = this->position->parent;
-					if (tmp == this->position->left)
-						break;
-					else 
-						tmp = this->position;
+				bst_node<T>	*tmp = this->position->parent;
+				while (tmp != NULL && this->position == tmp->right) { 
+					this->position = tmp;
+					tpm = tmp->parent;
 				}
-				if (this->position->parent == NULL)
-					return bst_iterator(NULL, this->tree);
+				this->position = tmp;
 			}
 			return *this;
-		}
+		};
+
+		bst_iterator		&operator++(int) {
+			bst_iterator temp = *this;
+			this->operator++();
+			return temp;
+		};
+
+		bst_iterator		&operator--() {
+			if (this->tree.empty()) {
+				throw std::underflow_error("Trying to decrement over an empty tree");
+			}
+			if (this->position == NULL) {
+				this->position = tree->root;
+				while (this->position->right != NULL) 
+					this->position = this->position->right;
+			}
+			else if (this->position->left != NULL) {
+				this->position = this->position->left;
+				while (this->position->right != NULL) 
+					this->position = this->position->right;
+			}
+			else {
+				bst_node<T>	*tmp = this->position->parent;
+				while (tmp != NULL && this->position == tmp->left) { 
+					this->position = tmp;
+					tpm = tmp->parent;
+				}
+				this->position = tmp;
+			}
+			return *this;
+		};
+
+		bst_iterator		&operator--(int) {
+			bst_iterator temp = *this;
+			this->operator--();
+			return temp;
+		};
 	};
 
 	template <typename T>
 	bool operator==(const bst_iterator<T>& lhs, const bst_iterator<T>& rhs) {
-		return (rhs.getPosition() == lhs.getPosition());
+		return (rhs.position == lhs.position && rhs.tree == lhs.tree);
 	};
 
 	template <typename T>
@@ -88,10 +120,54 @@ namespace ft {
 		private:
 		node	*root;
 
+		node	*_insert(const T &val, node *subtreeRoot, node *parent) {
+			if (subtreeRoot == NULL) {
+				subtreeRoot = new bst_node(val, NULL, NULL, parent);
+				return subtreeRoot;
+			}
+			else if (val < subtreeRoot->value)
+				return this->_insert(val, subtreeRoot->left, subtreeRoot);
+			else if (val > subtreeRoot->value)
+				return this->_insert(val, subtreeRoot->right, subtreeRoot);
+			else
+				return NULL;
+		};
+
+		bool	_remove(const T &val, node *subtreeRoot) {
+			if (subtreeRoot == NULL)
+				return false;
+			if (val < subtreeRoot->value)
+				return this->_remove(val, subtree->left);
+			if (val > subtreeRoot->value)
+				return this->_remove(val, subtree->right);
+			if (subtreeRoot->left != NULL && subtreeRoot->right != NULL) {
+				subtreeRoot->value = this->findMin(subtreeRoot->right)->value;
+				this->_remove(subtreeRoot->value, subtreeRoot->right);
+				return true;
+			}
+			else {
+				node *toBeDeleted = subtreeRoot;
+				subtreeRoot = (subtreeRoot->left != NULL) ? subtreeRoot->left : subtreeRoot->right;
+				subtreeRoot->parent = toBeDeleted->parent;
+				delete toBeDeleted;
+				return true;
+			}
+		};
+
+		node	*_clone(node *subtreeRoot) const {
+			if (subtreeRoot == NULL)
+				return NULL;
+			else
+				return new node(subtreeRoot->value, this->_clone(subtreeRoot->left),
+				this->_clone(subtreeRoot->right), subtreeRoot->parent);
+		};
+
 		public:
 		bs_tree() : root(NULL) {};
 		bs_tree(node *root) : root(root) {};
-		bs_tree(const bs_tree &other) {}; // clone instruction
+		bs_tree(const bs_tree &other) {
+			this->root = this->_clone(other.root);
+		};
 		~bs_tree() {
 			this->clear(this->root);
 		};
@@ -108,6 +184,8 @@ namespace ft {
 		};
 
 		node	*findMin() {
+			if (this->empty())
+				throw std::underflow_error("The tree is empty!");
 			node *res = this->root;
 			while (res != NULL)
 				res = res->left;
@@ -115,11 +193,31 @@ namespace ft {
 		};
 
 		node	*findMax() {
+			if (this->empty())
+				throw std::underflow_error("The tree is empty!");
 			node *res = this->root;
 			while (res != NULL)
 				res = res->right;
 			return (res);
 		};
+
+		const node	*find(const T &val) const {
+			node *res = this->root;
+			while (res != NULL || res->value != val) {
+				res = (res->value < val) res->left : res->right;
+			}
+			return res;
+		};
+
+		const_iterator	insert(const T &val) { 
+			node *res = this->_insert(val, this->root, NULL);
+			if (!res) { return this->end(); }
+			return bst_iterator(res, this);
+		};
+
+		void			remove(const T &val) {
+			this->_remove(val, this->root);
+		}
 
 		const_iterator	begin() const { 
 			return bst_iterator(this->findMin(), this);
